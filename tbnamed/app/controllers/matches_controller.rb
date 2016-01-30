32d15eb60f4a0cb553a1ff8f174da31772a7173f
Brian Bensch <bbensch@gmail.com>
2016-01-29 19:39:37 -0800

    copy over matches & swipes controller routes

--- tbnamed/app/controllers/matches_controller.rb
+++ tbnamed/app/controllers/matches_controller.rb
@@ -1,2 +1,17 @@
 class MatchesController < ApplicationController
+    # respond_to :json
+    # GET api/matches
+    def index
+      #return JSON of a user's matches profile information
+      user = User.find(2) #need to pull user_id from Devise
+      respond_with user.matches
+    end
+
+    # GET api/matches/:id
+    def show      
+      # Here is a new comment
+      # This should lead into chat mode
+      #return information needed for chat interface
+      respond_with Match.find(params[:id])
+    end
 end
