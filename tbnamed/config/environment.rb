26d90837a42f1defd4d4abda5304265bd095dfb8
Brian Bensch <bbensch@gmail.com>
2016-01-29 22:26:38 -0800

    FB auth key issue still DB

--- tbnamed/config/environment.rb
+++ tbnamed/config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Load environment variables for FB Authentication
+require 'dotenv'
+Dotenv.load
+
 # Initialize the Rails application.
 Rails.application.initialize!
