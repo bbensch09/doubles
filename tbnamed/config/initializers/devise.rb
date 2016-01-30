d04620510b45787dfa340e7e0759944ac4104dac
Brian Bensch <bbensch@gmail.com>
2016-01-29 22:26:38 -0800

    FB auth key issue still DB

--- tbnamed/config/initializers/devise.rb
+++ tbnamed/config/initializers/devise.rb
@@ -1,3 +1,6 @@
+require 'dotenv'
+Dotenv.load
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -233,7 +236,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"],
+  config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET'],
       callback_url: "http://localhost:3000/", scope: "email,user_location,public_profile", :display => 'popup', :image_size => 'square'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
