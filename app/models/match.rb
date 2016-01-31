56bc9a3d49bd83301e4ed88c057c237fbafd623b
mrYakamoto <wehmenstein@gmail.com>
2016-01-31 14:03:45 -0800

    add match screen placeholder, validation for duplicate match creation

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,7 @@
 class Match < ActiveRecord::Base
   belongs_to :first_user, :class_name => "User"
   belongs_to :second_user, :class_name => "User"
+
+  validates_uniqueness_of :first_user_id, :scope => :second_user_id
+
 end
