6e44e21700be1d5d2817fa9346c711bb4879a25f
Nil Thacker <nil@nil.gs>
2016-01-31 11:25:40 -0800

    Implement get_matched_user in matches helper

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,3 +1,5 @@
+include MatchesHelper
+
 class ConversationsController < ApplicationController
   def create
 
@@ -6,10 +8,6 @@ class ConversationsController < ApplicationController
   def show
     # get the matched user's info
     @match = Match.find(params[:id])
-    if @match.first_user_id == current_user.id
-      @matched_user = @match.second_user
-    elsif @match.second_user_id == current_user.id
-      @matched_user = @match.first_user
-    end
+    @matched_user = get_matched_user(@match, current_user)
   end
 end
