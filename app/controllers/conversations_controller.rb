a984445b108d3cbedfa45329e21ff66698268fc4
Nil Thacker <nil@nil.gs>
2016-01-31 13:52:48 -0800

    Implement updating of messages from server

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -9,18 +9,22 @@ class ConversationsController < ApplicationController
     # get the matched user's info
     @match = Match.find(params[:id])
     @matched_user = get_matched_user(@match, current_user)
-
-    # find all the messages for this match
-    @presenter = {
-      :messages => Message.where(match_id: @match.id).order('created_at ASC'),
-      :form => {
-        :action => "/matches/#{@match.id}/chat",
-        :csrf_param => request_forgery_protection_token,
-        :csrf_token => form_authenticity_token
-      }
-    }
+    @presenter = generate_presenter
     if request.xhr?
-      render :json => Message.all
+      render :json => generate_presenter
     end
+  end
+
+  private
+
+  def generate_presenter
+    return {
+          :messages => Message.where(match_id: @match.id).order('created_at ASC'),
+          :form => {
+            :action => "/matches/#{@match.id}/chat",
+            :csrf_param => request_forgery_protection_token,
+            :csrf_token => form_authenticity_token
+          }
+        }
   end
 end
