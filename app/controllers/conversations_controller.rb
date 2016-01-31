817e9872a7a004257fa67f12c02df82c7c140ab9
Nil Thacker <nil@nil.gs>
2016-01-31 13:29:19 -0800

    List of messages between users shows up on chat page

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -9,5 +9,18 @@ class ConversationsController < ApplicationController
     # get the matched user's info
     @match = Match.find(params[:id])
     @matched_user = get_matched_user(@match, current_user)
+
+    # find all the messages for this match
+    @presenter = {
+      :messages => Message.where(match_id: @match.id).order('created_at ASC'),
+      :form => {
+        :action => "/matches/#{@match.id}/chat",
+        :csrf_param => request_forgery_protection_token,
+        :csrf_token => form_authenticity_token
+      }
+    }
+    if request.xhr?
+      render :json => Message.all
+    end
   end
 end
