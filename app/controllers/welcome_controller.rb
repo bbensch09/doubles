422905df58cdc66512f7e2529de5fae0de5db348
Brian Bensch <bbensch@gmail.com>
2016-01-31 15:31:26 -0800

    modify welcome router to kick users to swipes immediately upon second and later logins

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
 
   def index
+    if current_user && current_user.sign_in_count == 1
+      redirect_to "/users/#{current_user.id}/profile"
+    elsif current_user.sign_in_count > 1
+      redirect_to "/users/#{current_user.id}/feed"
+    else
+      render 'index'
+    end
   end
 end
