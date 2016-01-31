7348ce4b23829ec754ca12af5d0fb93bf789e2ba
Brian Bensch <bbensch@gmail.com>
2016-01-31 14:29:16 -0800

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@
 #Tom (user1) plays all activities, and has swiped yes on all users.
 #Mark (user2) plays Racquetball, and has liked Tom, so they are a match.
 
-User.all.destroy
-Activity.all.destroy
-ActivityBlurb.all.destroy
-Swipe.all.destroy
-Match.all.destroy
-Message.all.destroy
+# User.delete_all
+Activity.delete_all
+ActivityBlurb.delete_all
+Swipe.delete_all
+Match.delete_all
+Message.delete_all
 
 # Create activities
 activity_names = [
@@ -67,29 +67,71 @@ ActivityBlurb.create!({
   # random = [true, false].sample
 
   user = User.create!({
-                    first_name: Faker::Name.first_name,
-                    last_name: Faker::Name.last_name,
-                    email: Faker::Internet.email,
-                    gender: ['male','female'].sample,
-                    age: rand(18..80),
-                    bio: Faker::Hipster.sentences(4).join(' '),
-                    location: "San Francisco",
-                    latitude: 37.7576792,
-                    longitude: -122.5078123,
-                    profile_picture_url: Faker::Avatar.image,
-                    password: 'password'
-                      })
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      gender: ['male','female'].sample,
+      age: rand(18..80),
+      bio: Faker::Hipster.sentences(4).join(' '),
+      location: "San Francisco",
+      latitude: 37.7576792,
+      longitude: -122.5078123,
+      profile_picture_url: Faker::Avatar.image,
+      password: 'password'
+        })
+end
 
+User.all.each do |user|
   ActivityBlurb.create!({
-      text: "sample text about activity",
-      activity_id: (1..10).to_a.sample
-      user_id: user.id
-      })
+    text: "sample text about activity",
+    activity_id: (1..10).to_a.sample,
+    user_id: user.id
+    })
   ActivityBlurb.create!({
-      text: "sample text about activity",
-      activity_id: (11..20).to_a.sample
-      user_id: user.id
-      })
+    text: "sample text about activity",
+    activity_id: (11..20).to_a.sample,
+    user_id: user.id
+    })
+end
+
+#Tom likes all users
+
+User.all.each do |swipee|
+  Swipe.create!({
+    swiped_yes: true,
+    swiper_id: 1,
+    swipee_id: swipee.id
+    })
+end
+
+
+#Mark likes Tom
+Swipe.create!({
+  swiped_yes: true,
+  swiper_id: 2,
+  swipee_id: 1
+  })
+
+#Mark sends Tom a message
+message = Message.create({
+  match_id: 1,
+  user_id: 2,
+  message_text: "Want to play Racquetball this weekend?"
+  # message_text: Faker::Hipster.sentences(2).join(' ')
+  })
+
+#everyone swipes on you (all but last user)
+def everyone_swipes_you
+  everyone = User.all
+  you = [User.last]
+  everyone_but_you = everyone - you
+  everyone_but_you.each do |user|
+      Swipe.create!({
+        swiped_yes: [true,false].sample,
+        swiper_id: user.id,
+        swipee_id: you.first.id
+        })
+  end
 end
 
 #create Brian
@@ -119,44 +161,112 @@ ActivityBlurb.create!({
     user_id: brian.id
     })
 
-#Brian likes Tom
-Swipe.create!({
-      swiped_yes: true,
-      swiper_id: brian.id,
-      swipee_id: tom.id
-      })
+#everyone swipes Brian
+everyone_swipes_you
 
-#Tom likes all users
+#create Nil
+nil_thacker = User.create!({
+                  first_name: "Nil",
+                  last_name: "Thacker",
+                  email: "nil@nil.gs",
+                  gender: "male",
+                  age: rand(25..35),
+                  bio: "I'm from LA. I like to code",
+                  location: "San Francisco",
+                  latitude: 37.7576792,
+                  longitude: -122.5078123,
+                  profile_picture_url: "https://scontent-lga3-1.xx.fbcdn.net/hprofile-xpa1/v/t1.0-1/c22.1.185.185/s160x160/250208_10150270016197149_6234017_n.jpg",
+                  password: 'password'
+                    })
 
-Users.all.each do |swipee_id|
-  Swipe.create!({
-    swiped_yes: true,
-    swiper_id: 1,
-    swipee_id: swipee_id
+ActivityBlurb.create!({
+    text: "I will crush you at foosball.",
+    activity_id: 1,
+    user_id: nil_thacker.id
     })
-end
+
+#everyone swipes Nil
+everyone_swipes_you
+
+#create Trevor
+trevor = User.create!({
+                  first_name: "Trevor",
+                  last_name: "Newcomb",
+                  email: "Tnewcomb0@gmail.com",
+                  gender: "male",
+                  age: rand(25..35),
+                  bio: "TBD",
+                  location: "Mountain View",
+                  latitude: 37.7576792,
+                  longitude: -122.5078123,
+                  profile_picture_url: "https://scontent-lga3-1.xx.fbcdn.net/hprofile-xpa1/v/t1.0-1/p160x160/11429970_10152944610704562_3007318549682082835_n.jpg",
+                  password: 'password'
+                    })
+
+ActivityBlurb.create!({
+    text: "Let's play some darts!",
+    activity_id: 1,
+    user_id: trevor.id
+    })
+
+#everyone swipes Trevor
+everyone_swipes_you
+
+#create Abe
+abe = User.create!({
+                  first_name: "Abe",
+                  last_name: "Clark",
+                  email: "abeaclark@gmail.com",
+                  gender: "male",
+                  age: rand(25..35),
+                  bio: "TBD",
+                  location: "Diamond Heights",
+                  latitude: 37.7576792,
+                  longitude: -122.5078123,
+                  profile_picture_url: "https://scontent-lga3-1.xx.fbcdn.net/hprofile-xlf1/v/t1.0-1/p160x160/12509300_10156395860765273_5982743154224695433_n.jpg",
+                  password: 'password'
+                    })
+
+ActivityBlurb.create!({
+    text: "I may not drink much, but when I play bar games, I crush.",
+    activity_id: 1,
+    user_id: abe.id
+    })
+
+#everyone swipes Greg
+everyone_swipes_you
+
+#create Greg
+greg = User.create!({
+                  first_name: "Greg",
+                  last_name: "Wehmeier",
+                  email: "wehmenstein@gmail.com",
+                  gender: "male",
+                  age: rand(25..35),
+                  bio: "TBD",
+                  location: "San Francisco",
+                  latitude: 37.7576792,
+                  longitude: -122.5078123,
+                  profile_picture_url: "https://scontent-lga3-1.xx.fbcdn.net/hprofile-xat1/v/t1.0-1/p160x160/12063360_10106705349412900_4054142801918383241_n.jpg",
+                  password: 'password'
+                    })
+
+ActivityBlurb.create!({
+    text: "Professional billiards champion.",
+    activity_id: 1,
+    user_id: greg.id
+    })
+
+#everyone swipes Greg
+everyone_swipes_you
 
 
-#Mark likes Tom
-Swipe.create!({
-  swiped_yes: true,
-  swiper_id: 2,
-  swipee_id: 1
-  })
-
-#Mark sends Tom a message
-message = Message.create({
-  match_id: 1,
-  user_id: 2,
-  message_text: "Want to play Racquetball this weekend?"
-  # message_text: Faker::Hipster.sentences(2).join(' ')
-  })
 
 #AUTOGENERATE MESSAGE WITH EACH MATCH from the original user
 Match.all.each do |match|
   Message.create!({
-  match_id: match.id
-  user_id: match.first_user_id
+  match_id: match.id,
+  user_id: match.first_user_id,
   message_text: "(This message is auto generated): It's cool to see we have something in common. Want to meet up?"
   })
 end
