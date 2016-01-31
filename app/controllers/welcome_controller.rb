2de0fac4553437aadbb32aa5595fd33c37a7f225
Brian Bensch <bbensch@gmail.com>
2016-01-31 15:50:39 -0800

    fix index controller logic for second login straigh to swiping

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   def index
     if current_user && current_user.sign_in_count == 1
       redirect_to "/users/#{current_user.id}/profile"
-    elsif current_user.sign_in_count > 1
+    elsif current_user && current_user.sign_in_count > 1
       redirect_to "/users/#{current_user.id}/feed"
     else
       render 'index'
