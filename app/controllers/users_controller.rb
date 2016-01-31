b3325b1a5ceb9cfe83c33a44df4fe494112c2286
Abe Clark <abeaclark@gmail.com>
2016-01-30 18:51:25 -0800

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def create
-    otouch Auth_info = request.env["omniauth.auth"]
+    oAuth_info = request.env["omniauth.auth"]
     p oAuth_info
     render :text => "Create user sports and things"
   end
