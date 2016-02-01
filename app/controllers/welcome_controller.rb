54b01c636cd03cb783b2453114eb353cd133afee
GregoryW <email": null>
2016-02-01 14:20:57 -0800

    modify geolocation method a little. not done yet

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,20 @@ class WelcomeController < ApplicationController
 
   def index
 
+
+
     if current_user && current_user.sign_in_count == 1
-      redirect_to "/profile"
-      elsif current_user && current_user.sign_in_count > 1
-      redirect_to "/feed"
+      if current_user.update_geolocation
+        redirect_to "/profile"
       else
+        flash[:show_modal] = true
+        flash[:modal_to_show] = "zipcode"
+        redirect_to "/profile"
+      end
+
+    elsif current_user && current_user.sign_in_count > 1
+      redirect_to "/feed"
+    else
       render 'index'
     end
   end
