5a980825271d374983fc60cf6906fc9ad744027a
Brian Bensch <bbensch@gmail.com>
2016-01-31 18:09:37 -0800

    update routes to be more virsually friendly; /feed /profile etc.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,9 @@ class WelcomeController < ApplicationController
   def index
     if current_user
     if current_user && current_user.sign_in_count == 1
-      redirect_to "/users/#{current_user.id}/profile"
+      redirect_to "/profile"
     elsif current_user && current_user.sign_in_count > 1
-      redirect_to "/users/#{current_user.id}/feed"
+      redirect_to "/feed"
     else
       render 'index'
     end
