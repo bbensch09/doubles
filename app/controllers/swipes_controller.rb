d99683ab7ebcbb6e5a44712884f5b9960a0f9a2d
Nil Thacker <nil@nil.gs>
2016-02-01 14:48:38 -0800

    Implment ajax call for swipes and required controller logic

--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -39,18 +39,27 @@ class SwipesController < ApplicationController
     end
 
     def swipe_yes
-
       new_swipe = current_user.swipes.create(swipee_id: params[:user_id], swiped_yes: true)
-      if ( User.find(params[:user_id]).swipes.where(swipee_id: current_user.id, swiped_yes: true).length > 0 )
-        @matched_user = User.find(params[:user_id])
-        render '/matches/overlay' and return
+      match_found = User.find(params[:user_id]).swipes.where(swipee_id: current_user.id, swiped_yes: true).length > 0
+
+      if request.xhr?
+        render :text => match_found ? "match!" : "yes"
       else
-        redirect_to "/feed"
+        if matched_user
+          @matched_user = User.find(params[:user_id])
+          render '/matches/overlay' and return if match_found
+        else
+          redirect_to "/feed"
+        end
       end
     end
 
     def swipe_no
-      current_user.swipes.create(swipee_id: params[:user_id],swiped_yes: false)
-      redirect_to "/feed"
+      current_user.swipes.create(swipee_id: params[:user_id], swiped_yes: false)
+      if request.xhr?
+        render :text => "no"
+      else
+        redirect_to "/feed"
+      end
     end
 end
