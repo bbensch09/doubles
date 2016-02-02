a728861b7eaed1af29561d6e8e534dea288a016b
mrYakamoto <wehmenstein@gmail.com>
2016-02-01 17:28:50 -0800

    add location by zip code and zip code modal

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@ class WelcomeController < ApplicationController
   include WelcomeHelper
 
   def index
-
     # first time user
     if current_user && current_user.sign_in_count == 1
       if current_user.update_lat_lng
@@ -24,9 +23,7 @@ class WelcomeController < ApplicationController
     else
       render 'index'
     end
-
   end
-
 
   def force_login
     render 'force_login'
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,7 +1,6 @@
 module WelcomeHelper
   def zip_code_modal
      flash[:show_modal] = true
-     flash[:modal_to_show] = "zipcode"
-     p "=========FLASHMODAL"
+     flash[:modal_to_show] = "/users/enter_zipcode"
   end
 end
