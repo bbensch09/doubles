cf92c01bd601c5c3ce5c2894b4840e86e355e8f0
Abe Clark <email": null>
2016-02-01 16:56:42 -0800

    Modified controller to only show sports that you don't already have on your profile

--- app/controllers/activity_blurbs_controller.rb
+++ app/controllers/activity_blurbs_controller.rb
@@ -7,14 +7,16 @@ class ActivityBlurbsController < ApplicationController
     end
 
     def new
-      @all_sports = Activity.all
+      @user = current_user
+      already_chosen_activities = @user.activities
+      @all_sports = Activity.all - already_chosen_activities
       @activity_blurb = ActivityBlurb.new
       render '/users/pick-sports'
     end
 
     def create
       @user = current_user
-      @activity_blurb = ActivityBlurb.new(activity_id: params[:activity_id], text: params[:text], user_id: @user.id)
+      @activity_blurb = ActivityBlurb.create(activity_id: params[:activity_id], text: params[:text], user_id: @user.id)
 
       if request.xhr?
         render json: @activity_blurb
